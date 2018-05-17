c2dfc19aa2b4b5a83e10b8fd26c89d579a077dfa
Austin Jensen <ajjensen1329@gmail.com>
2018-05-16 20:43:15 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className= 'banner'><h1>Make a New Appointment</h1></header>
+      </div>
+    );
+  }
+}
+
+export default App;
